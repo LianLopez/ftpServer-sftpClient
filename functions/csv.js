45d1a0633a012888dd18ba7fa7915d558f698327
LianLopez <lopez.leonardo.info@gmail.com>
2020-09-15 17:42:43 -0300

    csvToXls recibe un parametro string

--- functions/csv.js
+++ functions/csv.js
@@ -11,29 +11,26 @@ function csvParse(url) {
     return data;
 }
 
-function csvToXls(files) {
-    for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-        let src = `./ftp_files/orders/pending/${file}`;
-        let dataParsed = csvParse(src);
-        let buffer = xlsx.build([{ name: "pedidos", data: dataParsed }]);
-        let fileName = file;
-        fileName = fileName.split(".");
-        fileName.pop();
-        fileName = fileName.join();
-        fs.writeFile(
-            `./ftp_files/orders/processed/${fileName}.xlsx`,
-            buffer,
-            (err) => {
-                if (err) return console.error(err);
-                console.log(`${file} Creado exitosamente csvToXls`);
-            }
-        );
-        fs.unlink(src, (err) => {
-            if (err) throw err;
-            console.log(`${file} was deleted`);
-        });
-    }
+function csvToXls(file) {
+    let src = `./ftp_files/orders/pending/${file}`;
+    let dataParsed = csvParse(src);
+    let buffer = xlsx.build([{ name: "pedidos", data: dataParsed }]);
+    let fileName = file;
+    fileName = fileName.split(".");
+    fileName.pop();
+    fileName = fileName.join();
+    fs.writeFile(
+        `./ftp_files/orders/processed/${fileName}.xlsx`,
+        buffer,
+        (err) => {
+            if (err) return console.error(err);
+            console.log(`${file} Creado exitosamente csvToXls`);
+        }
+    );
+    fs.unlink(src, (err) => {
+        if (err) throw err;
+        console.log(`${file} was deleted`);
+    });
 }
 
 module.exports = {
